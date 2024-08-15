365cf1eb22e2c149b5a31e5dbb3c5f21f14fcd0b
uzair mashhadi <uzair.mashhadi@gmail.com>
2024-08-16 04:17:59 +0500

    handled expections

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -5,18 +5,26 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const isAdmin = async (req, res, next) => {
-    const email = req?.user?.email;
-    if (!email) {
-        return responseFormatter(res, STATUS_CODE.BAD_REQUEST, {}, TEXTS.someThingWentWrong);
+    try {
+        const email = req?.user?.email;
+        if (!email) {
+            return responseFormatter(res, STATUS_CODE.BAD_REQUEST, {}, TEXTS.someThingWentWrong);
+        }
+
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user) {
+            return responseFormatter(res, STATUS_CODE.UNAUTHORIZED, {}, TEXTS.userNotFound);
+        }
+
+        if (user.role === "CLIENT") {
+            return responseFormatter(res, STATUS_CODE.UNAUTHORIZED, {}, ERRORS.unAuthorized);
+        }
+
+        next();
+    } catch (error) {
+        console.error('Error in isAdmin middleware:', error);
+        responseFormatter(res, STATUS_CODE.INTERNAL_SERVER_ERROR, {}, TEXTS.internalServerError);
     }
-    const user = await prisma.user.findUnique({ where: { email } });
-    if (!user) {
-        return responseFormatter(res, STATUS_CODE.UNAUTHORIZED, {}, TEXTS.userNotFound);
-    }
-    if (user.role === "CLIENT") {
-        return responseFormatter(res, STATUS_CODE.UNAUTHORIZED, {}, ERRORS.unAuthorized);
-    }
-    next();
 };
 
 module.exports = isAdmin;
