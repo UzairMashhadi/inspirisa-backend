f8b497949f6aabc714ddc79437822320efa22ad4
uzair mashhadi <uzair.mashhadi@gmail.com>
2024-08-17 04:00:37 +0500

    fixed the accept data type to multipart form data and json

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const eventsRoutes = require('./routes/events');
+const usersRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
+const coursesRoutes = require('./routes/courses');
+const publicRoutes = require('./routes');
+const authMiddleware = require('./middleware/auth/authMiddleware');
+const CustomError = require("./utils/CustomError");
+const globalErrorHandler = require("./utils/errorHandler");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Auth routes
+app.use('/api', authRoutes);
+
+// User routes
+app.use('/api', authMiddleware, usersRoutes);
+
+// Events routes
+app.use('/api', authMiddleware, eventsRoutes);
+
+// Courses routes
+app.use('/api', authMiddleware, coursesRoutes);
+
+// Public
+app.use('/api', publicRoutes);
+
+app.get("/api/test", (req, res) => {
+    res.status(200).json({ message: "Server is working fine!" });
+});
+
+app.all("*", (req, res, next) => {
+    const err = new CustomError(
+        `Can't find ${req.originalUrl} on the server!`,
+        404
+    );
+    next(err);
+});
+
+app.use(globalErrorHandler);
+// Error handler (last middleware)
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,43 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+const http = require("http");
+const app = require("./app");
 const connectDB = require('./config/db');
-const globalErrorHandler = require('./utils/errorHandler');
-const eventsRoutes = require('./routes/events');
-const usersRoutes = require('./routes/users');
-const authRoutes = require('./routes/auth');
-const coursesRoutes = require('./routes/courses');
-const publicRoutes = require('./routes');
-const authMiddleware = require('./middleware/auth/authMiddleware');
+
+const server = http.Server(app);
 
 require('dotenv').config();
 
-const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Connect to the database
 connectDB();
 
-// Use CORS middleware
-app.use(cors());
+const unexpectedErrorHandler = (error) => {
+    console.log(error);
+    exitHandler();
+};
 
-app.use(express.json());
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
 
-// Auth routes
-app.use('/api', authRoutes);
+process.on("SIGTERM", () => {
+    console.log("SIGTERM received");
+    if (server) {
+        server.close();
+    }
+});
 
-// User routes
-app.use('/api', authMiddleware, usersRoutes);
 
-// Events routes
-app.use('/api', authMiddleware, eventsRoutes);
-
-// Courses routes
-app.use('/api', authMiddleware, coursesRoutes);
-
-// Public
-app.use('/api', publicRoutes);
-
-// Error handler (last middleware)
-app.use(globalErrorHandler);
-
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
